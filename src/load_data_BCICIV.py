09f9561da13d10e19f52b43e2d5cd69e74587f7d
EnekoIsturitzSesma <enekoisturitz@gmail.com>
2026-02-24 13:54:38 +0100

    multiband preprocessing

--- src/load_data_BCICIV.py
+++ src/load_data_BCICIV.py
@@ -63,7 +63,70 @@ def prepare_motor_imagery_dataset(gdf_file, t_start=2.0, t_end=6.0):
     return data
 
 
-def load_all_subjects(data_dir, stage='T'):
+def prepare_motor_imagery_dataset_multiband(gdf_file, t_start=2.0, t_end=6.0, bands=[(8, 12), (13, 30)]):
+    raw = mne.io.read_raw_gdf(gdf_file, preload=True, verbose=False)
+    fs = int(raw.info['sfreq'])
+    
+    events, event_dict = mne.events_from_annotations(raw, verbose=False)
+    left_mne_id = event_dict.get('769', None) 
+    right_mne_id = event_dict.get('770', None) 
+    
+    if left_mne_id is None or right_mne_id is None:
+        raise ValueError(f"No events found in: {event_dict}")
+        
+    left_events = events[events[:, 2] == left_mne_id]
+    right_events = events[events[:, 2] == right_mne_id]
+
+    channels_to_use = ['EEG-0', 'EEG-4', 'EEG-5', 'EEG-C3', 'EEG-6', 'EEG-Cz', 'EEG-7','EEG-C4', 'EEG-8', 'EEG-9', 'EEG-13']
+    raw.pick(channels_to_use)
+    
+    n_samples_trial = int((t_end - t_start) * fs)
+    start_idx = int(t_start * fs)
+    
+    X_all_bands = [] 
+    y = [] 
+    
+    for (l_freq, h_freq) in bands:
+        raw_band = raw.copy().filter(l_freq, h_freq, fir_design='firwin', verbose=False)
+        signal = raw_band.get_data() 
+        
+        X_band = []
+        y_band = []
+        
+        for event_pos in left_events[:, 0]:
+            trial_start = event_pos + start_idx
+            trial_end = trial_start + n_samples_trial
+            if trial_end <= signal.shape[1]:
+                X_band.append(signal[:, trial_start:trial_end])
+                y_band.append(0)
+                
+        for event_pos in right_events[:, 0]:
+            trial_start = event_pos + start_idx
+            trial_end = trial_start + n_samples_trial
+            if trial_end <= signal.shape[1]:
+                X_band.append(signal[:, trial_start:trial_end])
+                y_band.append(1)
+        
+        X_all_bands.append(np.array(X_band))
+        
+        if len(y) == 0:
+            y = np.array(y_band)
+            
+    X = np.stack(X_all_bands, axis=1)
+    
+    data = {
+        'X': X,
+        'y': y,
+        'fs': fs,
+        'n_channels': len(channels_to_use),
+        'n_bands': len(bands),
+        'info': f"Trials: {len(y)} | Left: {(y==0).sum()} | Right: {(y==1).sum()} | Shape: {X.shape}"
+    }
+    
+    return data
+
+
+def load_all_subjects(data_dir, stage='T', use_multiband=False, bands=[(8, 12), (13, 30)]):
     files = os.listdir(data_dir)
     subject_ids = sorted(list(set([f[0:3] for f in files if f.endswith(f'{stage}.gdf')])))
     
@@ -79,7 +142,10 @@ def load_all_subjects(data_dir, stage='T'):
             continue
         
         try:
-            data = prepare_motor_imagery_dataset(gdf_file)
+            if use_multiband:
+                data = prepare_motor_imagery_dataset_multiband(gdf_file, bands=bands)
+            else:
+                data = prepare_motor_imagery_dataset(gdf_file)
             X_all.append(data['X'])
             y_all.append(data['y'])
             subject_all.extend([subj_id] * len(data['y']))
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,22 +1,32 @@
 import numpy as np
 import copy
 
-def laplacian_filter(X, channels, neighbours):
+def laplacian_filter(X, channels, neighbours, use_multiband=False):
 
     laplacian = copy.deepcopy(X)
 
-    for channel, n in zip(channels, neighbours):
-        laplacian[:, channel, :] = X[:, channel, :] - np.mean(X[:, n, :], axis=1)
+    if use_multiband:
+        n_bands = X.shape[1]
+        for b in range(n_bands):
+            for channel, n in zip(channels, neighbours):
+                laplacian[:, b, channel, :] = X[:, b, channel, :] - np.mean(X[:, b, n, :], axis=1)
+    else:
+        for channel, n in zip(channels, neighbours):
+            laplacian[:, channel, :] = X[:, channel, :] - np.mean(X[:, n, :], axis=1)
 
     return laplacian
 
 
-def channel_aggregation(X):
+def channel_aggregation(X, use_multiband=False):
 
     aggregated = copy.deepcopy(X)
 
-    aggregated = np.mean(aggregated, axis=1, keepdims=True)
+    if use_multiband:
+        n_bands = X.shape[1]
+        for b in range(n_bands):
+            aggregated = np.mean(X, axis=2, keepdims=True)
+    else:
+        aggregated = np.mean(aggregated, axis=1, keepdims=True)
 
     return aggregated
-
 
